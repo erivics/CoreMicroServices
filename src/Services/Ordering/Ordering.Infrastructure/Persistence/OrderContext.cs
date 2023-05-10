77b3ea3af51b59be61cfff8019c68a916b1c6114
erivics <adesinakbayo@gmail.com>
2023-05-10 17:01:23 +0100

    DbContext

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> option) : base(option)
+        {
+        }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
