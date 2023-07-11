29eb436cd90285902ca7c0457bf9be8973fbe33f
Kolawole Adesina <kolawole.adesina@mplify.com>
2023-07-11 20:44:38 +0100

    Ocelot config

--- src/ApiGateways/OcelotApiGateway/Program.cs
+++ src/ApiGateways/OcelotApiGateway/Program.cs
@@ -3,6 +3,14 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOcelot();
+
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json",true, true);
+
+//builder.Host.ConfigureAppConfiguration(configDelegate =>
+//{
+//    configDelegate.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json",true, true);
+//});
+
 builder.Services.AddLogging(configure =>
 {
     configure.AddConfiguration(builder.Configuration.GetSection("Logging"));
