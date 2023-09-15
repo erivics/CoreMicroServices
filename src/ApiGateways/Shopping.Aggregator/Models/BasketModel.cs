c16cf3e55f890cd43d815a7429c295a6686c02ef
erivics <adesinakbayo@gmail.com>
2023-09-15 14:36:20 +0100

    DevelopDTO for Aggregator pattern

---
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    public class BasketModel
+    {
+        public string UserName { get; set; }
+        public List<BasketItemExtendedModel> Items = new List<BasketItemExtendedModel>();
+        public decimal TotalPrice { get; set; }
+    }
+}
